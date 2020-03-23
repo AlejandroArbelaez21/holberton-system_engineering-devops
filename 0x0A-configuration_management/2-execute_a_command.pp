03933f8613670a47c93bab9c6550c64b036e3d1c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-23 16:37:25 -0500

    learning about of puppet, very interesting

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kill the process named killmenow
 exec { 'kill_process':
   command => 'pkill killmenow',
-  path    => '/Holberton/holberton-system_engineering-devops/0x0A-configuration_management',
+  path    => '/usr/local/bin/:/bin/',
 }
