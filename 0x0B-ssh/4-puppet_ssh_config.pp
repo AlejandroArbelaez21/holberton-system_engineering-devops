35c839ff557aa33ca8d34cd14e9f3458c4e8a387
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-24 19:54:49 -0500

    advanced

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #create a file in /tmp with puppet
-file_line {'Declare identity file':
-        path    => '/home/alejandro/.ssh/ssh_config',
-        line    => "IdentityFile ~/.ssh/holberton\n",
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/home/alejandro/.ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line {'Turn off passwd auth':
-        path    => '/home/alejandro/.ssh/ssh_config',
-        line    => "IdentityFile ~/.ssh/holberton\n",
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/home/alejandro/.ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
 }
-
