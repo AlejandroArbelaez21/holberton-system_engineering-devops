516ca888d9726e744e7160528c24854ed9b66d22
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-26 23:14:13 -0500

    advanced

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 #This script containg commands to automatically configure an Ubuntu machine to respect above requirements
 exec { 'install_nginx_puppet':
-  command  => "apt-get -y update && apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.nginx-debian.html && sed -i '/listen 80 default_server;/a\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-enabled/default && service nginx start",
-  provider => 'shell',
+  command  => "sudo apt-get -y update && sudo apt-get -y install nginx && echo 'Holberton School' > /var/www/html/index.nginx-debian.html && sudo sed -i '/listen 80 default_server;/a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-enabled/default && sudo service nginx start",
+  provider => "shell",
 }
