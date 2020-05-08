93001450afeb308929c6a4d2c156bd14f034a227
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-07 21:43:45 -0500

    learning about API advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles of the first 10 hot posts
+"""
+
+import requests
+
+def top_ten(subreddit):
+    head = {'user-agent': 'X-Modhash'}
+    page = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    r = requests.get(page, headers=head, allow_redirects=False, params={'limit': 10})
+    r_json = r.json()
+    if r.status_code == 404:
+        print(None)
+    else:
+        for display in r_json['data']['children']:
+            print(display['data']['title'])
