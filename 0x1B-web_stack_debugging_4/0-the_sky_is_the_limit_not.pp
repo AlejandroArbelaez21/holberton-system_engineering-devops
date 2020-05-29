c6f5a8e2f0cd487743d5adfc825e14ba742d9fd3
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-28 23:46:13 -0500

    Debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,9 @@
-#In this web stack debugging task, we are testing how well our web server setup featuring Nginx 
-
+#test of Debugging
 exec {'sky is the limit':
   provider => shell,
   command  => 'sed -i "s/15/2000/g" /etc/default/nginx',
 }
-
-exec {'restart ngnix':
+exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
