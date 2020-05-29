cd41616af59be7b8c048c1dc290eb97c66bea709
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-28 23:13:02 -0500

    Debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 #In this web stack debugging task, we are testing how well our web server setup featuring Nginx 
-#For the benchmarking, we are using ApacheBench which is a quite popular tool in the industry
-exec {'sky_is_the_limit':
+
+exec {'sky is the limit':
   provider => shell,
-  command  => "sed -i 's/15/2000/g' /etc/default/nginx",
+  command  => 'sed -i "s/15/2000/g" /etc/default/nginx',
 }
 
-exec {'restart_ngnix':
+exec {'restart ngnix':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
