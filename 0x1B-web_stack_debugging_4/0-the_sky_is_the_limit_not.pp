cc6b7366dd132436d2d7320e616d43c95147cef8
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-28 23:58:03 -0500

    the final file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #change the wrong number of request
-exec {'change request number':
+exec {'change_number_of_request':
   provider => shell,
   command  => 'sed -i "s/15/2000/g" /etc/default/nginx',
 }
