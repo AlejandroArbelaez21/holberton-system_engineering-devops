99163ebbfc0a905c20c38e5c728ecd642e08c53f
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-28 23:50:36 -0500

    pleaseeeeee

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-#test of Debugging
-exec {'sky is the limit':
+#change the wrong number placed in a file
+exec {'change number of request':
   provider => shell,
   command  => 'sed -i "s/15/2000/g" /etc/default/nginx',
 }
